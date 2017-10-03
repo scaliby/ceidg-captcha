a772bff2ecc0819201259be271e72823d2e79ff3
scaliby <scaliby@gmail.com>
2017-10-03 09:42:43 +0200

    Removed hardcoded values

--- ceidg-captcha-rest/src/main/java/net/scaliby/ceidgcaptcha/rest/converter/impl/BufferedImageToINDArrayConverterImpl.java
+++ ceidg-captcha-rest/src/main/java/net/scaliby/ceidgcaptcha/rest/converter/impl/BufferedImageToINDArrayConverterImpl.java
@@ -1,6 +1,7 @@
 package net.scaliby.ceidgcaptcha.rest.converter.impl;
 
 import net.scaliby.ceidgcaptcha.machinelearning.factory.ImageTransformFactory;
+import net.scaliby.ceidgcaptcha.machinelearning.resource.ImageTransformConfigurationResource;
 import net.scaliby.ceidgcaptcha.rest.converter.BufferedImageToINDArrayConverter;
 import net.scaliby.ceidgcaptcha.rest.exception.ImageProcessingException;
 import org.datavec.image.loader.NativeImageLoader;
@@ -14,17 +15,22 @@ import java.io.IOException;
 public class BufferedImageToINDArrayConverterImpl implements BufferedImageToINDArrayConverter {
 
     private final ImageTransformFactory imageTransformFactory;
+    private final ImageTransformConfigurationResource imageTransformConfigurationResource;
 
     @Inject
-    public BufferedImageToINDArrayConverterImpl(ImageTransformFactory imageTransformFactory) {
+    public BufferedImageToINDArrayConverterImpl(ImageTransformFactory imageTransformFactory, ImageTransformConfigurationResource imageTransformConfigurationResource) {
         this.imageTransformFactory = imageTransformFactory;
+        this.imageTransformConfigurationResource = imageTransformConfigurationResource;
     }
 
     @Override
     public INDArray convert(BufferedImage bufferedImage) {
         try {
             ImageTransform transform = imageTransformFactory.create();
-            return new NativeImageLoader(32, 32, 1, transform).asMatrix(bufferedImage);
+            int height = imageTransformConfigurationResource.getScaledHeight();
+            int width = imageTransformConfigurationResource.getScaledWidth();
+            int channels = imageTransformConfigurationResource.getChannels();
+            return new NativeImageLoader(height, width, channels, transform).asMatrix(bufferedImage);
         } catch (IOException ex) {
             throw new ImageProcessingException(ex);
         }
